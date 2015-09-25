57782d430a9141243d4bff33affb8f49ad4cb4e1
Alberto Molina <alberto.molina1103@gmail.com>
2015-09-24 20:42:47 -0600

    Iniciando

--- CiudadSoS/app/src/main/java/com/etps3/ciudadsos/ciudadsos/InicioActivity.java
+++ CiudadSoS/app/src/main/java/com/etps3/ciudadsos/ciudadsos/InicioActivity.java
@@ -4,13 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class InicioActivity extends AppCompatActivity {
 
+    ImageView phone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+        phone=(ImageView)findViewById(R.id.imagePhone);
+        phone.setImageResource(R.drawable.microphone);
     }
 
     @Override
