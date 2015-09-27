5b27f3715d82af60079d8400ba6f3e22796e8418
Inerbus <alberto.molina1103@gmail.com>
2015-09-26 21:36:29 -0600

    Modificando
     En la rama Alberto
     Your branch is up-to-date with 'origin/Alberto'.
    
     Cambios para hacer commit:
    	deleted:    CiudadSoS/.idea/dbnavigator.xml
    	deleted:    CiudadSoS/.idea/encodings.xml
    	modified:   CiudadSoS/.idea/gradle.xml
    	modified:   CiudadSoS/.idea/misc.xml
    	modified:   CiudadSoS/app/src/main/java/com/etps3/ciudadsos/ciudadsos/InicioActivity.java
    	deleted:    CiudadSoS/app/src/main/res/drawable/Bomberos.jpg
    	deleted:    CiudadSoS/app/src/main/res/drawable/Cruz_Roja.png
    	deleted:    CiudadSoS/app/src/main/res/drawable/Cruz_verde.jpg
    	deleted:    CiudadSoS/app/src/main/res/drawable/Hospitales.jpg
    	deleted:    CiudadSoS/app/src/main/res/drawable/PNC.png
    	deleted:    CiudadSoS/app/src/main/res/drawable/Proteccion_Civil.jpg
    	new file:   CiudadSoS/app/src/main/res/drawable/fondo.png
    	modified:   CiudadSoS/app/src/main/res/layout/activity_inicio.xml
    	modified:   CiudadSoS/app/src/main/res/values/strings.xml

--- CiudadSoS/app/src/main/java/com/etps3/ciudadsos/ciudadsos/InicioActivity.java
+++ CiudadSoS/app/src/main/java/com/etps3/ciudadsos/ciudadsos/InicioActivity.java
@@ -1,23 +1,74 @@
 package com.etps3.ciudadsos.ciudadsos;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageView;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class InicioActivity extends AppCompatActivity {
 
-    ImageView phone;
 
-
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+    private ImageButton btnSpeak;
+    private TextView mensaje ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
 
-        phone=(ImageView)findViewById(R.id.imagePhone);
-        phone.setImageResource(R.drawable.microphone);
+        mensaje=(TextView)findViewById(R.id.txtmensaje);
+        btnSpeak=(ImageButton)findViewById(R.id.speakButton);
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                promptSpeechInput();
+            }
+        });
+
+    }
+
+    private void promptSpeechInput()
+    {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    mensaje.setText(result.get(0));
+                }
+                break;
+            }
+
+        }
     }
 
     @Override
