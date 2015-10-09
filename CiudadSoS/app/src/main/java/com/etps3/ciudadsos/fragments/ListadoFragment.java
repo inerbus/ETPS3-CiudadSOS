544f7dcfde9d5e392f5fed78499998fe21db37c1
Alberto Molina <alberto.molina1103@gmail.com>
2015-10-08 22:48:34 -0600

    Modificando lista personalizada de entidades 2

--- CiudadSoS/app/src/main/java/com/etps3/ciudadsos/fragments/ListadoFragment.java
+++ CiudadSoS/app/src/main/java/com/etps3/ciudadsos/fragments/ListadoFragment.java
@@ -1,12 +1,15 @@
 package com.etps3.ciudadsos.fragments;
 
 import android.graphics.Color;
+import android.support.annotation.ColorInt;
+import android.support.annotation.ColorRes;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.etps3.ciudadsos.adaptadores.EntidadesListAdapter;
@@ -28,6 +31,12 @@ public class ListadoFragment extends Fragment {
 
         ArrayList<Entidad> datos= getListEntidades();
         lst.setAdapter(new EntidadesListAdapter(rootView.getContext(), datos));
+        lst.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
         return rootView;
     }
 
